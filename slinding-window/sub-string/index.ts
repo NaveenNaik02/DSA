f5942e13914c4ec08c6ce53e509299239ed19ac9
Naveen Naik <79866255+NaveenNaik02@users.noreply.github.com>
2025-10-16 09:40:03 +0000

    chore: updated substring probelm using sliding window

--- slinding-window/sub-string/index.ts
+++ slinding-window/sub-string/index.ts
@@ -1,20 +1,22 @@
-function longestSubstringWithoutRepeat(s: string): number {
+const longestSubstring = (s: string): number => {
   let maxLength = 0;
   let start = 0;
   const map = new Map<string, number>();
 
   for (let end = 0; end < s.length; end += 1) {
-    if (map.has(s[end]) && map.get(s[end])! >= start) {
-      start = map.get(s[end])! + 1;
+    const index = map.get(s[end])
+
+    if(index !== undefined && index >= start){
+      start = index + 1;
     }
 
     map.set(s[end], end);
-
-    maxLength = Math.max(maxLength, end - start + 1);
+    maxLength = Math.max(maxLength, end - start + 1)
   }
   return maxLength;
 }
 
 // Test cases
-console.log(longestSubstringWithoutRepeat("abcabcbb"));
-console.log(longestSubstringWithoutRepeat("abba"));
+console.log(longestSubstring("baaccbcbb"));
+console.log(longestSubstring("abcabcbb"));
+console.log(longestSubstring("abba"));
